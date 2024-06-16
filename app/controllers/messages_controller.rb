e959f18187778d0e007a7c8ec9ff82dd7c725584
Tomoya Yoshida <ihatov08@gmail.com>
2024-06-16 18:15:44 +0900

    message修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,18 +2,7 @@ class MessagesController < ApplicationController
   def create
     message = Message.new(message_params)
     message.save!
-    message = if message.body.include?('今日の運勢')
-                %w[大吉 中吉 小吉 吉 凶].sample
-              elsif message.body.include?('天気')
-                %w[晴れ 曇り 雨 雪].sample
-              elsif message.body == 'こんにちは'
-                'こんにちは'
-              elsif message.body == '元気づけて'
-                %w[頑張れ！ あきらめないで！ いつも応援してるよ！].sample
-              else
-                %w[こんにちは こんばんは おはようございます].sample
-              end
-    bot_chat = Message.new(body: message, bot: true)
+    bot_chat = Message.new(body: bot_message, bot: true)
     bot_chat.save!
   end
 
@@ -21,5 +10,9 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def bot_message
+    'プログラミングをマスターしてお金と時間と場所の自由を手に入れよう'
   end
 end
