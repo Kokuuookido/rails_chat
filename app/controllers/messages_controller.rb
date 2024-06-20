c4426da111aa722a420212f3b45814fdd91a032a
Kokuuookido <122447698+Kokuuookido@users.noreply.github.com>
2024-06-20 16:35:20 +0000

    練習問題

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,6 +13,31 @@ class MessagesController < ApplicationController
   end
 
   def bot_message
-    'こんばんわ'
+    if message_params[:body] == "トヨタ"
+      "日本車です。"
+    elsif message_params[:body] == "ホンダ"
+      "日本車です"
+    elsif message_params[:body] == "BMW"
+      "ドイツ車です。"
+    elsif message_params[:body] == "メルセデスベンツ"
+      "ドイツ車です。"
+    else
+      "わかりません。"
+    end
+    def bot_message
+      if message_params[:body] == "春"
+        "たけのこ"
+      elsif message_params[:body] == "夏"
+        "すいか"
+      elsif message_params[:body] == "秋"
+        "栗"
+      elsif message_params[:body] == "冬"
+"みかん"
+      else
+        "未対応のメッセージです"
+      end
+    end
+    
   end
+  
 end
--- practice/04.rb
+++ practice/04.rb
@@ -1,4 +1,7 @@
 juice = "コーラ"
+# =で名札を付ける。言い換えると、変数に代入する。
 puts juice
-price=1
+price = 1
 puts price
+変数
+オブジェクトの名札のようなもの
